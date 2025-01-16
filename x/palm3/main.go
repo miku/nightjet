0e8940116b72b44c26f4004fb77bd440e7f65bba
Martin Czygan <martin.czygan@gmail.com>
2025-01-16 02:09:48 +0100

    palm3: wip

--- x/palm3/main.go
+++ x/palm3/main.go
@@ -6,40 +6,46 @@ import (
 	"time"
 )
 
-// ANSI escape codes for colors
-const (
-	reset = "\033[0m"
-	green = "\033[32m"
-	brown = "\033[33m"
-)
+// ANSI color codes for green shades
+var greenShades = []string{
+	"\033[38;5;28m", // Dark green
+	"\033[38;5;34m", // Medium green
+	"\033[38;5;40m", // Light green
+	"\033[38;5;46m", // Bright green
+}
 
-// Randomize leaf patterns
-func randomLeaf() string {
-	leafPatterns := []string{
-		"   /  |  \\",
-		"  /   |   \\",
-		"  \\   |   /",
-		"   \\__|__/",
+const reset = "\033[0m"
+
+func randomShade() string {
+	return greenShades[rand.Intn(len(greenShades))]
+}
+
+func printPalmTree() {
+	rand.Seed(time.Now().UnixNano())
+
+	// Leaves (top of the tree)
+	for i := 0; i < 6; i++ {
+		padding := 6 - i
+		line := ""
+		for j := 0; j < i*2+1; j++ {
+			line += randomShade() + "█" + reset
+		}
+		fmt.Printf("%s%s\n", spaces(padding), line)
 	}
-	return leafPatterns[rand.Intn(len(leafPatterns))]
+
+	// Trunk (middle of the tree)
+	for i := 0; i < 8; i++ {
+		fmt.Printf("%s%s\n", spaces(6), "\033[38;5;94m█\033[0m") // Brownish trunk
+	}
+
+	// Ground (base of the tree)
+	fmt.Printf("\033[38;5;94m%s\n%s\n%s\033[0m\n", spaces(4)+"███", spaces(3)+"█████", spaces(2)+"███████")
+}
+
+func spaces(count int) string {
+	return fmt.Sprintf("%*s", count, "")
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
-	// Palm tree leaves
-	fmt.Println(green + "    __ __" + reset)
-	for i := 0; i < 4; i++ {
-		fmt.Println(green + randomLeaf() + reset)
-	}
-
-	// Trunk
-	for i := 0; i < 6; i++ {
-		fmt.Println(brown + "      |" + reset)
-	}
-
-	// Base
-	fmt.Println(brown + "     / \\" + reset)
-	fmt.Println(brown + "    /   \\" + reset)
-	fmt.Println(brown + "   /     \\" + reset)
+	printPalmTree()
 }
