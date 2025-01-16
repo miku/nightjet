19f56d98d8f1463b9a5074e3d72fcf758238246b
Martin Czygan <martin.czygan@gmail.com>
2025-01-16 02:09:19 +0100

    palm3: wip

--- x/palm3/main.go
+++ x/palm3/main.go
@@ -1,25 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func main() {
-	palmTree := []string{
-		"    __ __",
-		"   /  V  \\",
+// ANSI escape codes for colors
+const (
+	reset = "\033[0m"
+	green = "\033[32m"
+	brown = "\033[33m"
+)
+
+// Randomize leaf patterns
+func randomLeaf() string {
+	leafPatterns := []string{
+		"   /  |  \\",
 		"  /   |   \\",
 		"  \\   |   /",
 		"   \\__|__/",
-		"      |",
-		"      |",
-		"      |",
-		"      |",
-		"      |",
-		"     / \\",
-		"    /   \\",
-		"   /     \\",
+	}
+	return leafPatterns[rand.Intn(len(leafPatterns))]
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	// Palm tree leaves
+	fmt.Println(green + "    __ __" + reset)
+	for i := 0; i < 4; i++ {
+		fmt.Println(green + randomLeaf() + reset)
 	}
 
-	for _, line := range palmTree {
-		fmt.Println(line)
+	// Trunk
+	for i := 0; i < 6; i++ {
+		fmt.Println(brown + "      |" + reset)
 	}
+
+	// Base
+	fmt.Println(brown + "     / \\" + reset)
+	fmt.Println(brown + "    /   \\" + reset)
+	fmt.Println(brown + "   /     \\" + reset)
 }
