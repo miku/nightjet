741cf1e3e26321348c0ecd1b92dbb489fd55106d
Martin Czygan <martin.czygan@gmail.com>
2025-01-16 02:21:30 +0100

    palm4: wip

--- x/palm4/main.go
+++ x/palm4/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
 
 	"github.com/fatih/color"
@@ -11,24 +12,24 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	// Create some colors for the fronds
-	colors := []*color.Color{
+	// Create some shades of green for the leaves
+	greens := []*color.Color{
+		color.New(color.FgHiGreen),
 		color.New(color.FgGreen),
-		color.New(color.FgYellow),
-		color.New(color.FgMagenta),
-		color.New(color.FgCyan),
 	}
 
-	// Draw the trunk of the palm tree
-	for i := 0; i < 3; i++ {
+	// Draw the stem of the palm tree
+	for i := 0; i < 2; i++ {
 		fmt.Println(" |")
 	}
 
-	// Draw the fronds of the palm tree
+	// Draw the leaves of the palm tree
 	for i := 0; i < 5; i++ {
-		// Choose a random color for this frond
-		c := colors[rand.Intn(len(colors))]
-		// Print the frond in the chosen color
-		c.Println(" /\\")
+		// Choose a random shade of green for this leaf
+		g := greens[rand.Intn(len(greens))]
+		// Create the leaf using solid green block characters
+		leaf := strings.Repeat("█", i+1)
+		// Print the leaf in the chosen color
+		g.Println(leaf)
 	}
 }
