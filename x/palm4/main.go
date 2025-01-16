7635a658e0d99dc5d9d649928e4f797b111f4fba
Martin Czygan <martin.czygan@gmail.com>
2025-01-16 02:20:39 +0100

    palm4: wip

--- x/palm4/main.go
+++ x/palm4/main.go
@@ -1,15 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	// Create some colors for the fronds
+	colors := []*color.Color{
+		color.New(color.FgGreen),
+		color.New(color.FgYellow),
+		color.New(color.FgMagenta),
+		color.New(color.FgCyan),
+	}
+
 	// Draw the trunk of the palm tree
 	for i := 0; i < 3; i++ {
-		fmt.Println("  |")
+		fmt.Println(" |")
 	}
 
 	// Draw the fronds of the palm tree
 	for i := 0; i < 5; i++ {
-		fmt.Println(" /\\")
+		// Choose a random color for this frond
+		c := colors[rand.Intn(len(colors))]
+		// Print the frond in the chosen color
+		c.Println(" /\\")
 	}
 }
